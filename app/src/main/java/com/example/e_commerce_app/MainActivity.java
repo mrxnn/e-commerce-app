cbc8df6e122b1b91e24fc64cc08497a596827530
mayura <mayura.ramanayake@gmail.com>
2022-09-07 18:31:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/e_commerce_app/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.e_commerce_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // hide action bar
+        if (getSupportActionBar() != null)
+            getSupportActionBar().hide();
+
+        // hide status bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
