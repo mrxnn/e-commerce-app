fb8e44d036bd53d5496bc826cd40650c0149a769
mayura <mayura.ramanayake@gmail.com>
2022-10-24 05:34:51 +0530

    Firebase Auth - Register Users

--- app/src/main/java/com/example/e_commerce_app/RegisterActivity.java
+++ app/src/main/java/com/example/e_commerce_app/RegisterActivity.java
@@ -1,17 +1,28 @@
 package com.example.e_commerce_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Patterns;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
     TextView loginLink;
     Button registerButton;
+    Button submitButton;
+    TextInputEditText emailInput;
+    TextInputEditText passwordInput;
+    TextInputEditText confirmPasswordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +36,53 @@ public class RegisterActivity extends AppCompatActivity {
         // hide status bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        // login link click
+        // init ui elements
         loginLink = findViewById(R.id.login_link);
-        loginLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-            }
-        });
-
-        // register button click
         registerButton = findViewById(R.id.register_button);
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        submitButton = findViewById(R.id.submit_button);
+        emailInput = findViewById(R.id.emailInputEditText);
+        passwordInput = findViewById(R.id.passwordInputEditText);
+        confirmPasswordInput = findViewById(R.id.confirmPasswordInputEditText);
+
+        // attach event handlers
+        loginLink.setOnClickListener((view -> loginToAccount()));
+        registerButton.setOnClickListener((view) -> registerAccount());
+        submitButton.setOnClickListener(view -> registerAccount());
+    }
+
+    void loginToAccount() {
+
+    }
+
+    void registerAccount() {
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        String confirmPassword = confirmPasswordInput.getText().toString();
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailInput.setError("Invalid email");
+            return;
+        }
+
+        if (password.length() < 6) {
+            passwordInput.setError("Password must be 6 or more characters");
+            return;
+        }
+
+        if (!password.equals(confirmPassword)) {
+            confirmPasswordInput.setError("Passwords do not match");
+            return;
+        }
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(RegisterActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
